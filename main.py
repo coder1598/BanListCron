035c384d07eb5ab2ddb3a1a29bb1da3eadf25700
Noufal Nazar <naufalnazar@icloud.com>
2025-05-07 18:30:38 +0000

    fix-403-error

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ SLEEP_TIME = 3
 REQUEST_TIMEOUT = 10
 
 REQ_HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br, zstd',
